e2a4d446121c0a8636dde91fcf0102a3e92845a4
AishikMondal <85011447+AishikMondal@users.noreply.github.com>
2025-10-20 02:06:07 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/HBDSreeja',     // change to your repo name
+  assetPrefix: '/HBDSreeja/',
+};
+
+module.exports = nextConfig;
